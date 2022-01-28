f4bd4fdfccfc734376bce9a18a544fb7b9830540
Ramraj <ramrage13@gmail.com>
2022-01-28 09:46:25 -0800

    student com iut

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const courseController = require('../controllers/courseController.js')
+const courseController = require('../controllers/courseController.js');
+const studentController = require('../controllers/studentController');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -12,4 +13,12 @@ router.post('/courses/edit/:id', courseController.updateCourse);
 router.get('/courses/add', courseController.renderAddForm);
 router.post('/courses/add', courseController.addCourse);
 router.get('/courses/delete/:id', courseController.deleteCourse);
+
+router.get('/students', studentController.viewAll);
+router.get('/students/profile/:id', studentController.viewProfile);
+router.get('/students/edit/:id', studentController.renderEditForm);
+router.post('/students/edit/:id', studentController.updateStudent);
+router.get('/students/add', studentController.renderAddForm);
+router.post('/students/add', studentController.addStudent);
+router.get('/students/delete/:id', studentController.deleteStudent);
 module.exports = router;
